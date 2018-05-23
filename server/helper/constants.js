5d5f4b44b4ce87b7dd760fb16e6f4547fae2e491
joelwass <joel.wasserman8@gmail.com>
2018-05-23 05:42:08 -0400

    added in product model

--- server/helper/constants.js
+++ server/helper/constants.js
@@ -1,4 +1,3 @@
 module.exports = {
-  RECOMMENDATION_CATEGORIES: ['books', 'movies', 'restaurants', 'miscellaneous', 'food', 'activities', 'places', 'music', 'apps', 'games', 'websites'],
-  RECOMMENDATION_STATUS_TYPES: ['pending', 'resolved']
+  
 }
---
+++ server/models/product.js
@@ -0,0 +1,104 @@
+const constants = require('../helper/constants')
+
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    image: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    price: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    price_dollars: {
+      type: DataTypes.INTEGER,
+      required: true
+    },
+    price_cents: {
+      type: DataTypes.INTEGER,
+      required: true
+    },
+    discount: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    discount_dollars: {
+      type: DataTypes.INTEGER,
+      required: true
+    },
+    discount_cents: {
+      type: DataTypes.INTEGER,
+      required: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    meta: {
+      type: DataTypes.JSON
+    }
+  }, {
+    freezeTableName: true,
+    indexes: [
+      {
+        unique: true,
+        fields: ['id']
+      }
+    ],
+    hooks: {
+      // afterUpdate: (recommendation, options) => {
+      //   // figure out of the recommendation was correct or not
+      //   const correctRecommendation = recommendation.prediction === recommendation.result
+      //   // find the to_user and update the to_users recommendationsReceivedCorrect field and the from_users recomendation correct field
+      //   sequelize.models.User.findOne({ where: { id: recommendation.to_user } })
+      //     .then(user => {
+      //       if (correctRecommendation) user.recommendationsReceivedCorrect++
+      //       return user.save()
+      //     })
+      //     .then(() => sequelize.models.User.findOne({ where: { id: recommendation.from_user } }))
+      //     .then(user => {
+      //       if (correctRecommendation) user.recommendationsGivenCorrect++
+      //       return user.save()
+      //     })
+      //     .catch(err => {
+      //       return Promise.reject(err)
+      //     })
+      // },
+      // beforeCreate: (recommendation, options) => {
+      //   // update the from_users recommendations_given field and the to_users recommendations received field
+      //   sequelize.models.User.findOne({ where: { id: recommendation.to_user } })
+      //     .then(user => {
+      //       user.recommendationsReceived++
+      //       return user.save()
+      //     })
+      //     .then(() => sequelize.models.User.findOne({ where: { id: recommendation.from_user } }))
+      //     .then(user => {
+      //       user.recommendationsGiven++
+      //       return user.save()
+      //     })
+      //     .catch(err => {
+      //       return Promise.reject(err)
+      //     })
+      // }
+    }
+  })
+
+  return Product
+}
