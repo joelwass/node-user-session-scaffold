9247fa57d8f3b5baafc382528660cdebea2f646f
Joel Wasserman <joel.wasserman8@gmail.com>
2018-08-28 10:32:30 -0700

    removed jwt

--- helper/index.js
+++ helper/index.js
@@ -1,5 +1,4 @@
 module.exports.strings = require('./strings')
 module.exports.methods = require('./methods')
 module.exports.CustomError = require('./error')
-module.exports.auth = require('./auth')
 module.exports.constants = require('./constants')
